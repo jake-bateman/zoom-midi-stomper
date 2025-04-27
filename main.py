5504ab16a65acc6af51cddc0008471c76482b7b1
jake <jake@nowhere>
2025-04-27 22:03:28 +0100

    fix scroll more

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def recover():
 
 
 def set_led(bool):
-    
+
     if bool:
         subprocess.run(["pinctrl", "set", "25", "op", "dh"])
     else:
@@ -109,79 +109,69 @@ def main():
         set_led(0)
         print("PRANG: No ZOOM pedal found on the MIDI bus.")
         midi_dev = recover()
-    
+
     # assume that at this point all is well and set the LED on
     set_led(1)
     enable_parameter_editing(midi_dev)
-    
+
     # poorly named variables. "last known value of the pin connected to the increment switch"
     # and "last known value of the pin connected to the decrement switch" is what they are.
     last_increment = get_pin_state(GPIO_INCREMENT)
     last_decrement = get_pin_state(GPIO_DECREMENT)
-    
+
     tick = 0
     print("watching for switch presses...")
     while True:
         val_increment = get_pin_state(GPIO_INCREMENT)
         val_decrement = get_pin_state(GPIO_DECREMENT)
-        
-        # MIDI connection status check every 30 ticks and set LED accordingly 
-        if tick % 30:
-            if find_midi_device() == None:
+
+        if tick % 30 == 0:
+            if find_midi_device() is None:
                 set_led(0)
             else:
                 set_led(1)
 
+        if val_decrement == "lo" and last_decrement == "hi":
+            patch = get_patch_number(midi_dev)
+            if patch is not None:
+                new_patch = (patch - 1) % 50
+                set_patch_number(midi_dev, new_patch)
+                print(f"decremented patch to {new_patch}")
+
+            scroll_tick = 0
+            while get_pin_state(GPIO_DECREMENT) == "lo":
+                if scroll_tick > 50 and scroll_tick % 15 == 0:
+                    patch = get_patch_number(midi_dev)
+                    if patch is not None:
+                        new_patch = (patch - 1) % 50
+                        set_patch_number(midi_dev, new_patch)
+                        print(f"decremented patch to {new_patch}")
+                scroll_tick += 1
+                time.sleep(sleep_time)
+
         if val_increment == "lo" and last_increment == "hi":
             patch = get_patch_number(midi_dev)
-
-            # increment, wrapping at 49
             if patch is not None:
                 new_patch = (patch + 1) % 50
                 set_patch_number(midi_dev, new_patch)
                 print(f"incremented patch to {new_patch}")
 
-        if val_decrement == "lo" and last_decrement == "hi":
-            patch = get_patch_number(midi_dev)
+            scroll_tick = 0
+            while get_pin_state(GPIO_INCREMENT) == "lo":
+                if scroll_tick > 50 and scroll_tick % 15 == 0:
+                    patch = get_patch_number(midi_dev)
+                    if patch is not None:
+                        new_patch = (patch + 1) % 50
+                        set_patch_number(midi_dev, new_patch)
+                        print(f"incremented patch to {new_patch}")
+                scroll_tick += 1
+                time.sleep(sleep_time)
 
-            # decrement, wrapping at 49
-            if patch is not None:
-                new_patch = (patch - 1) % 50
-                set_patch_number(midi_dev, new_patch)
-                print(f"decremented patch to {new_patch}")
-       
-        # inc/dec repeatedly if switch is held
-        scroll_tick = 0
+        last_increment = val_increment
+        last_decrement = val_decrement
+        tick += 1
+        time.sleep(sleep_time)
 
-        while get_pin_state(GPIO_DECREMENT) == "lo":
-            if scroll_tick % 50 == 0:
-                scrolling = True
-
-            if scrolling and scroll_tick % 15 == 0:
-                patch = get_patch_number(midi_dev)
-                if patch is not None:
-                    new_patch = (patch - 1) % 50
-                    set_patch_number(midi_dev, new_patch)
-                    print(f"decremented patch to {new_patch}")
-            scroll_tick += 1
-            time.sleep(sleep_time)
-
-        scroll_tick = 0
-        scrolling = False
-
-        while get_pin_state(GPIO_INCREMENT) == "lo":
-            if scroll_tick % 50 == 0:
-                scrolling = True
-
-            if scrolling and scroll_tick % 15 == 0:
-                patch = get_patch_number(midi_dev)
-                if patch is not None:
-                    new_patch = (patch + 1) % 50
-                    set_patch_number(midi_dev, new_patch)
-                    print(f"incremented patch to {new_patch}")
-            scroll_tick += 1
-            time.sleep(sleep_time)
 
 if __name__ == "__main__":
     main()
-
